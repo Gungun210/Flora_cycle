c2312e25bbbae5d4bb2c476de8601399deab9313
srija <srija@srijas-MacBook-Air.local>
2026-03-20 14:41:31 +0530

    Added React version of Navbar and Footer

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import "./style.css";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="logo">🌸 Floral Cycle</div>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ src/components/Petals.jsx
@@ -0,0 +1,17 @@
+import "./style.css";
+
+function Petals() {
+  return (
+    <div className="petals">
+      <span>🌸</span>
+      <span>🌼</span>
+      <span>🌺</span>
+      <span>🌸</span>
+      <span>🌼</span>
+      <span>🌺</span>
+      <span>🌹</span>
+    </div>
+  );
+}
+
+export default Petals;
